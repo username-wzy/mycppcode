5f2be65b798633b65ceebabebeb55be26c23c824
username-wzy <wangzhongyu20131016@outlook.com>
2026-01-22 18:24:42 +0800

    modified:   .gitignore 	new file:   P1659.cpp 	modified:   extra-kmp.cpp 	modified:   "manacher\346\261\202\346\234\200\345\244\247\345\233\236\346\226\207\345\255\220\344\270\262.cpp" 	deleted:    tt.cpp

--- extra-kmp.cpp
+++ extra-kmp.cpp
@@ -1,3 +1,22 @@
+/*
+ * Z算法（扩展KMP，Extended KMP）
+ *
+ * Z算法是一种字符串匹配算法，用于快速求出一个字符串的Z函数。
+ * Z函数定义：z[i] 表示字符串 s[i...n] 与 s[1...n-i+1] 的最长公共前缀长度。
+ *
+ * 应用场景：
+ * - 判断一个字符串是否是另一个字符串的子串
+ * - 求字符串的最长公共前缀
+ * - 求字符串的最长 border（既是前缀又是后缀的子串）
+ * - 字符串周期性分析
+ *
+ * 算法复杂度：O(n)，其中 n 为字符串长度
+ *
+ * 参考资料：
+ * - OI Wiki: https://oi-wiki.org/string/z-func/
+ * - 洛谷题解: https://www.luogu.com.cn/problem/solution/P5410
+ */
+
 #include <bits/stdc++.h>
 #define ll long long
 
@@ -7,27 +26,53 @@ int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(0), cout.tie(0);
+
+    // Z算法核心变量
+    // [l, r] 表示当前最右的匹配区间，即 s[l...r] 与 s[1...r-l+1] 匹配
     int l = 1, r = 0;
+
     string s;
     cin >> s;
+
+    // 在字符串前面加一个空格，使下标从1开始，方便处理
     s = ' ' + s;
-    int n = s.size() - 1;
+    int n = s.size() - 1; // 字符串实际长度
+
+    // z[i] 表示 s[i...n] 与 s[1...n-i+1] 的最长公共前缀长度
     int* z = new int[n + 5];
+
+    // 从第2个位置开始计算Z值（因为 z[1] = n，即整个字符串与自己匹配）
     for (int i = 2; i <= n; i++) {
+        // 如果当前位置i在当前最右匹配区间 [l, r] 内
+        // 我们可以利用之前计算的信息来加速
         if (i <= r) {
+            // k 是 i 在匹配区间 [l, r] 中对应的对称位置
+            // 因为 s[i...r] 与 s[k...r-l+1] 匹配，所以 z[i] 至少为 z[k]
             int k = i - l + 1;
+            // 取 z[k] 和剩余区间长度 r-i+1 的较小值
+            // 这样可以避免超出区间范围
             z[i] = min(z[k], r - i + 1);
         }
+
+        // 从当前已知的匹配长度开始，逐个字符向后扩展
+        // 继续比较直到不匹配为止
         while (i + z[i] <= n && s[z[i] + 1] == s[i + z[i]])
             ++z[i];
+
+        // 如果当前匹配区间比之前的更靠右，更新 l 和 r
+        // 这样可以保证我们总是使用最右的匹配信息
         if (i + z[i] - 1 > r) {
             l = i, r = i + z[i] - 1;
         }
     }
+
+    // 找出所有 Z 值中的最大值
+    // 这个最大值表示字符串中最长的 border（既是前缀又是后缀的子串）长度
     int ans = 0;
     for (int i = 1; i <= n; i++) {
         ans = max(ans, z[i]);
     }
+
     cout << ans;
     return 0;
 }
--- manacher求最大回文子串.cpp
+++ manacher求最大回文子串.cpp
@@ -1,36 +1,84 @@
+/*
+ * Manacher算法（马拉车算法）
+ *
+ * Manacher算法是一种用于在线性时间内找出字符串中最长回文子串的算法。
+ * 传统暴力枚举中心点的方法时间复杂度为O(n²)，而Manacher算法通过利用回文串的对称性，
+ * 将时间复杂度优化到O(n)。
+ *
+ * 核心思想：
+ * 1. 预处理字符串：在字符之间插入特殊字符（如#），使所有回文子串都变成奇数长度
+ * 2. 维护最右回文边界：记录当前已知的回文子串的最右边界R及其中心M
+ * 3. 利用对称性：对于当前位置i，如果i在当前最右回文边界内，可以利用对称点的回文半径信息
+ * 4. 向外扩展：在利用对称信息的基础上，继续向外扩展以找到最大的回文半径
+ *
+ * 应用场景：
+ * - 求字符串的最长回文子串
+ * - 统计字符串中所有回文子串的数量
+ * - 回文串相关的问题
+ *
+ * 算法复杂度：O(n)，其中 n 为字符串长度
+ *
+ * 参考资料：
+ * - OI Wiki: https://oi-wiki.org/string/manacher/
+ * - 洛谷题解: https://www.luogu.com.cn/problem/solution/P3805
+ */
+
 #include <bits/stdc++.h>
 #define ll long long
 
 using namespace std;
 
+// Manacher算法实现：返回字符串s的最长回文子串
 string manacher(string& s)
 {
+    // 预处理：在字符之间插入#，使所有回文子串都变成奇数长度
+    // 例如："aba" -> "#a#b#a#"
     string t = "#";
     for (char ch : s) {
-        t += ch, t += "#"; // 用#分割，使所有字符情况下
+        t += ch, t += "#";
     }
-    int M = 0, R = 0, n = t.size(); // 分别为对称中心点，和最大右边界
-    int maxLen = 0, maxCenter = 0; // 最大长度与中心点
+
+    // M: 当前最右回文子串的中心位置
+    // R: 当前最右回文子串的右边界（不包含）
+    int M = 0, R = 0, n = t.size();
+
+    // maxLen: 最长回文子串的半径长度
+    // maxCenter: 最长回文子串的中心位置
+    int maxLen = 0, maxCenter = 0;
+
+    // p[i]: 以位置i为中心的回文子串的半径长度
     vector<int> p(n, 0);
+
     for (int i = 0; i < n; i++) {
-        int k = 2 * M - i; // 算对称点下标
-        // 利用对称性快速算初始半径
+        // 计算i关于M的对称点k的位置
+        int k = 2 * M - i;
+
+        // 如果i在当前最右回文边界R内，利用对称性快速计算初始半径
+        // 取对称点的半径和到R的距离的最小值，避免超出边界
         if (i < R) {
             p[i] = min(R - i, p[k]);
         }
-        // 尝试扩展
+
+        // 尝试向外扩展回文子串
+        // 从当前已知半径的下一个位置开始比较
         int l = i - (1 + p[i]), r = i + (1 + p[i]);
         while (l >= 0 && r < n && t[l] == t[r]) {
-            p[i]++;
-            l--, r++;
+            p[i]++; // 扩展成功，半径+1
+            l--, r++; // 继续向外扩展
         }
-        if (i + p[i] > R) {
+
+        // 如果当前回文子串超过了最右边界R，更新M和R
+        if (i + p[i] > R)
             M = i, R = i + p[i];
-        }
-        // 记录最大值
+
+        // 记录最大回文子串的信息
         if (p[i] > maxLen)
             maxLen = p[i], maxCenter = i;
     }
+
+    // 计算最长回文子串在原字符串中的起始位置
+    // 在预处理后的字符串中，原字符串的字符下标都是偶数位置
+    // 所以需要除以2来转换回原字符串的下标
     int start = (maxCenter - maxLen) / 2;
     return s.substr(start, maxLen);
 }
@@ -42,6 +90,8 @@ int main()
 
     string s;
     cin >> s;
+
+    // 输出最长回文子串的长度
     cout << manacher(s).size();
 
     return 0;
